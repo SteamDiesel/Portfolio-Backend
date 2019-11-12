3e107fec39ee465564f22747b0d80b16ef217ca9
Jason Law <jason.lloyd.law@gmail.com>
2019-11-12 22:23:44 +1000

    installed laravel/passport, bootstrap, vue.js and got welcome page to display vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
 require('./bootstrap');
+import Vue from "vue";
+import 'es6-promise/auto';
+
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue').default
+);
+
+
+const app = new Vue({
+    el: '#app',
+  });
